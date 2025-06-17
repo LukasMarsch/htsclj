039654deea45ed372303b8be471fd9ee931674d1
Lukas Marsch <marschlukas@gmail.com>
2025-06-18 00:58:22 +0200

    rename artifact

--- build.clj
+++ build.clj
@@ -1,7 +1,7 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'my/lib1)
+(def lib 'clojure/htsclj)
 (def version (format "0.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
